f41c96c49abc91549d3e5bad955ffbcc1e04a002
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-05-11 17:51:55 +0800

    Preview the tables and its columns
    Using:SHOW TABLES & DESCRIBE
    Result:there are 6 tables:College
    Course
    CourseCatalog
    Day
    Gender
    Period
    Student
    TimeSlot

--- a4_q1.sql
+++ a4_q1.sql
@@ -1,8 +1,14 @@
 -- Select YOUR own database for assignment 4;
 
 USE a4_courses_b08601030;
+SHOW TABLES;
 
-DESCRIBE CourseCatalog;
 DESCRIBE College;
-
+DESCRIBE Course;
+DESCRIBE CourseCatalog;
+DESCRIBE Day;
+DESCRIBE Gender;
+DESCRIBE Period;
+DESCRIBE Student;
+DESCRIBE TimeSlot;
 -- Populate CourseCatalog table
