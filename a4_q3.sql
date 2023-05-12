b167c28482d3e54b4021bd5e2fdb7c9b41f17e06
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-05-12 18:39:24 +0800

    Populate the StudentCourse table
    Using: INSERT VALUES
    to insert the StudentId and CourseCatelogId represent which student have choose which course
    and if we want the details of the course can join to Course table

--- a4_q3.sql
+++ a4_q3.sql
@@ -18,15 +18,44 @@ VALUES
 CREATE TABLE StudentCourse (
   StudentCourseId INT NOT NULL AUTO_INCREMENT,
   StudentId INT NOT NULL,
-  CourseId INT NOT NULL,
+  CourseCatalogId INT NOT NULL,
   PRIMARY KEY (StudentCourseId),
   KEY FK_StudentId (StudentId),
-  KEY FK_CourseId (CourseId),
+  KEY FK_CourseId (CourseCatalogId),
   CONSTRAINT FK_StudentId FOREIGN KEY (StudentId) REFERENCES Student (StudentId),
-  CONSTRAINT FK_CourseId FOREIGN KEY (CourseId) REFERENCES Course (CourseId)
+  CONSTRAINT FK_CourseCatalogId1 FOREIGN KEY (CourseCatalogId) REFERENCES CourseCatalog (CourseCatalogId)
 );
+DROP TABLE StudentCourse;
 DESCRIBE StudentCourse;
 
 -- Populate StudentCourse table
+INSERT INTO StudentCourse(StudentId,CourseCatalogId)
+VALUES
+(1,207),
+(1,199),
+(1,173),
+(1,222),
+(1,245),
+(2,191),
+(2,171),
+(2,182),
+(2,318),
+(2,220),
+(3,190),
+(3,171),
+(3,200),
+(3,289),
+(3,211),
+(4,250),
+(4,171),
+(4,183),
+(4,318),
+(4,222),
+(4,233),
+(5,173),
+(5,193),
+(5,309),
+(5,262),
+(5,275)
 
-
+SELECT * FROM StudentCourse;
