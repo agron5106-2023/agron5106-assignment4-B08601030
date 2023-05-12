42063e009294c3cfd415ee9a866eed687a71e58f
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-05-12 18:17:42 +0800

    Fix the typo and create a new table Studentcourse
    There is a typo in primary key of Student
    Using: ALTER CHANGE to fix it
    
    Using:CREATE to create StudentCourse
    with StudentCourseId -->primary key
         StudentId-->foreign key
         CourseId-->foreign key

--- a4_q3.sql
+++ a4_q3.sql
@@ -12,8 +12,20 @@ VALUES
   ('John', 2, 103),
   ('Tom', 4, 102);
 
--- CREATE StudentCourse table
+ ALTER TABLE Student CHANGE COLUMN StuedentId StudentId INT NOT NULL AUTO_INCREMENT;
 
+-- CREATE StudentCourse table
+CREATE TABLE StudentCourse (
+  StudentCourseId INT NOT NULL AUTO_INCREMENT,
+  StudentId INT NOT NULL,
+  CourseId INT NOT NULL,
+  PRIMARY KEY (StudentCourseId),
+  KEY FK_StudentId (StudentId),
+  KEY FK_CourseId (CourseId),
+  CONSTRAINT FK_StudentId FOREIGN KEY (StudentId) REFERENCES Student (StudentId),
+  CONSTRAINT FK_CourseId FOREIGN KEY (CourseId) REFERENCES Course (CourseId)
+);
+DESCRIBE StudentCourse;
 
 -- Populate StudentCourse table
 
