a192ea8e5c63401b6f729800bc6b990599ce7bfa
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-05-12 20:59:00 +0800

    Find the course number and name of each student taken
    Using:JOIN the StudentCourse with Sudent and CatelogCourse
    to get the course number and course name of each student taken

--- a4_q4.sql
+++ a4_q4.sql
@@ -1,4 +1,14 @@
 -- Select YOUR own database for assignment 4;
 -- USE A4_courses_YOUR_NTU_ID;
+SELECT 
+NickName,
+CourseNumber,
+Coursename
+FROM StudentCourse 
+JOIN Student 
+ON StudentCourse.StudentId =Student.StudentId 
+JOIN CourseCatalog
+ON StudentCourse.CourseCatalogId =CourseCatalog.CourseCatalogId; 
+
 
 
