d46a3e69d83ca6693abd7ad31c8f31834ff9e890
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-05-12 15:29:53 +0800

    Set a table with TimeSlotOneId can be NULL
    Using:SHOW CREATE TABLE,DROP TABLE, CREATE TABLE
    let the TimeSlotOneId can be NULL
    
    because their is a online course and a course without timeslot on the Course website.

--- a4_q2.sql
+++ a4_q2.sql
@@ -2,12 +2,26 @@
 -- USE A4_courses_YOUR_NTU_ID;
 
 DESCRIBE Course;
+SHOW CREATE TABLE Course;
+DROP TABLE Course;
+CREATE TABLE `Course` (
+  `CourseId` int NOT NULL AUTO_INCREMENT,
+  `CourseCatalogId` int NOT NULL,
+  `TimeSlotOneId` int  NULL,
+  PRIMARY KEY (`CourseId`),
+  KEY `FK_CourseCatalogId` (`CourseCatalogId`),
+  KEY `FK_CourseTimeSlotOne` (`TimeSlotOneId`),
+  CONSTRAINT `FK_CourseCatalogId` FOREIGN KEY (`CourseCatalogId`) REFERENCES `CourseCatalog` (`CourseCatalogId`),
+  CONSTRAINT `FK_CourseTimeSlotOne` FOREIGN KEY (`TimeSlotOneId`) REFERENCES `TimeSlot` (`TimeSlotId`)
+) 
+
+SELECT * FROM Course;
 
 SELECT * FROM TimeSlot 
 JOIN Day ON TimeSlot.DayId = Day.DayId
 JOIN Period ON TimeSlot.PeriodId = Period.PeriodId;
 #LIMIT 5;
-
+SELECT * FROM Day;
 -- Modify/ALTER Course table
 
 
