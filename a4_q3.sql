fd29b5569517745e60f1400342a6a18079433e89
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-05-12 17:40:38 +0800

    Explore the tables Student,Gender
    Using:DESCRIBE, SELECT * FROM table_name

--- a4_q3.sql
+++ a4_q3.sql
@@ -1,8 +1,12 @@
 -- Select YOUR own database for assignment 4;
 -- USE A4_courses_YOUR_NTU_ID;
-
+DESCRIBE Student;
+DESCRIBE Gender;
+SELECT * FROM Student;
+SELECT * FROM Gender;
 -- CREATE StudentCourse table
 
 
 -- Populate StudentCourse table
+
 
